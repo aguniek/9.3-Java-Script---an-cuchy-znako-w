4681efbeafde2eb575123a78456f5aa89c481913
Agnieszka Szczepkowska <agnieszka.szczepkowska.work@gmail.com>
2018-05-09 20:22:48 +0200

    additional tasks

--- scripts.js
+++ scripts.js
@@ -5,3 +5,71 @@ var result1 = dinosaur.toUpperCase();
 var result2 = text.replace('Velociraptor', result1);
 
 console.log(result2.split(0, result2.length / 2));
+
+/* dodatkowe:
+- stworz funkcje, która przyjmuje za argumenty
+a) stringa
+b) liczbę wyrazów (X)
+i zwraca pierwsze X wyrazów ze stringa */
+
+function xFirstWords(txt, x) {
+    return txt.
+    split(' ').
+    slice(0, x).
+    join(' ');
+}
+
+/*- stwórz funkcję która przyjmuje za argument stringa i zwraca go "od tyłu" => zmienia np. "auto" na "otua" */
+
+function reverseString(txt) {
+    return txt.
+    split('').
+    reverse().
+    join('');
+}
+
+/*- stworz funkcję, która z podanego stringa usunie WSZYSTKIE spacje i zwróci jeden ciąg wyrazów bez spacji*/
+
+function replaceString(txt) {
+    return txt.
+    split(' ').
+    join('');
+}
+
+/*- stwórz funkcję, która zmieni podanego stringa na 'TaKiE cOś' */
+
+function camelString(txt) {
+    return txt.
+    split('').
+    map(
+        function (value, index) {
+            return index % 2 === 0 ? value.toUpperCase() : value.toLowerCase();
+        }
+    ).join('');
+}
+
+
+/*- stwórz funkcję która przyjmuje argument
+    a) stringa
+    b) literę
+    i zwraca tablicę pozycji (indexy) WSZYSTKICH wystąpień tej litery */
+
+function indexString(txt, leter) {
+    return txt.
+    split('').
+    map(
+        function (value, index) {
+            return [value, index];
+        }
+    ).
+    filter(
+        function (arr) {
+            return arr[0] == leter;
+        }
+    ).
+    map(
+        function (arr) {
+            return arr[1];
+        }
+    );
+}
